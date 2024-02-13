8d0ea0e07f5b634f600acc8bb11e38c3ab887e68
VasuVibingPrivetly <143987026+vasuvibingprivetly@users.noreply.github.com>
2024-02-13 18:45:22 +0530

    home_page.dart

--- exam_app/lib/views/screens/home_page.dart
+++ exam_app/lib/views/screens/home_page.dart
@@ -1,11 +1,10 @@
-import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:exam_app/utils/product_utils.dart';
-
+import '../../utils/product_utils.dart';
 import '../../utils/route_utils.dart';
+import '../components/category_product.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -16,12 +15,12 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.sizeOf(context);
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        actions: [
-          const Padding(
+        actions: const [
+          Padding(
             padding: EdgeInsets.all(10),
           ),
         ],
@@ -30,41 +29,29 @@ class _HomePageState extends State<HomePage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              ...allCategories
-                  .map(
+              ...allCategories.map(
                     (e) => GestureDetector(
                   onTap: () {
-                    log("CAT: $e");
+                    debugPrint("CAT: $e");
                     Navigator.pop(context);
                     selectedCategory = e;
                     setState(() {});
                   },
                   child: Row(
                     children: [
-                      CircleAvatar(
-                        backgroundImage: NetworkImage(
-                            ImageList[allCategories.indexOf(e)]),
-                      ),
-                      Container(
-                        color: Colors.grey,
-                      ),
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
-                          e.replaceFirst(
-                            e[0],
-                            e[0].toUpperCase(),
-                          ),
+                          e.replaceFirst(e[0], e[0].toUpperCase()),
                           style: const TextStyle(
-                            fontSize: 20,
+                            fontSize: 18,
                           ),
                         ),
                       ),
                     ],
                   ),
                 ),
-              )
-                  .toList(),
+              ).toList(),
             ],
           ),
         ),
@@ -78,7 +65,7 @@ class _HomePageState extends State<HomePage> {
               height: size.height * 0.20,
               width: size.width * 10,
               decoration: BoxDecoration(
-                  color: Colors.red, borderRadius: BorderRadius.circular(10)),
+                  color: Colors.amber, borderRadius: BorderRadius.circular(10)),
             ),
             SizedBox(
               height: size.height * 0.02,
@@ -101,6 +88,11 @@ class _HomePageState extends State<HomePage> {
                         label: const Text("Clear filter"),
                       ),
                     ),
+                    if (selectedCategory != null)
+                      CategoryProduct(
+                        category: selectedCategory!,
+                        context: context,
+                      ),
                   ],
                 ),
               ),
